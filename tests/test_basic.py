d89fb579c7b93f009aad45ec3062a0076a00178c
katakras <katakrask@gmail.com>
2018-01-05 00:54:47 +0000

    more tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,8 +4,11 @@ import unittest
 class MyTestCase(unittest.TestCase):
 
     # tests that something is actually being run
-    def test_something(self):
+    def test_basic(self):
         self.assertEqual(True, True)
+
+    def test_accuracy(self):
+        self.assertAlmostEqual(1.00002, 1.00003, 3)
 
 
 if __name__ == '__main__':
