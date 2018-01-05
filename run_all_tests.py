e26432efb599350411ced76862caef7036694a19
katakras <katakrask@gmail.com>
2018-01-05 00:36:56 +0000

    tests 6 commit

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,6 +1,5 @@
 
 import unittest
-import test
 
 #suite = unittest.TestLoader().loadTestsFromModule(test)
 #unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -2,8 +2,10 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+
+    # tests that something is actually being run
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
