d66e9cd3e70a9047d7fe707acbbcbba5405b324f
katakras <katakrask@gmail.com>
2018-01-07 14:07:45 +0000

    more fixes, added setup

--- setup.py
+++ setup.py
@@ -22,3 +22,14 @@ class PyTest(TestCommand):
         import pytest
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
+
+setup(
+   name='EcxAnx',
+   version='1.0',
+   description='Econometrics and analytics for time series.',
+   license='MIT',
+   author='Luis Huesca Molina',
+   author_email='katakrask@gmail.com',
+   packages=['EcxAnx'],
+   install_requires=['numpy', 'pandas'],
+)
